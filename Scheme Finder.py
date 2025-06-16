5d98f1f51e087704236c154e47c4d834b9a5b559
Tanav-Kolar <tanavk04@gmail.com>
2025-06-17 00:17:47 +0530

    Update Scheme Finder.py

--- Scheme Finder.py
+++ Scheme Finder.py
@@ -3,24 +3,52 @@ from pinecone_utils import query_index
 from groq import Groq
 import os
 from dotenv import load_dotenv
+import asr
+import sounddevice as sd
+import numpy as np
+import translate
+
 
 # Load environment variables
 load_dotenv()
-
 # Setup Groq client
 groq_client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+# Setup ASR
+DURATION = 10 # seconds
+SAMPLING_RATE = 16000
+
 
 st.set_page_config(page_title="Boudhi Chatbot", layout="centered")
 st.title("💬 Ask Boudhi – Your Scheme Assistant")
 
+
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
+if "state" not in st.session_state:
+    st.session_state.state = {}
 
-query = st.chat_input("Ask about schemes, benefits, eligibility...")
+#Store empty transcript string
+transcript = ""
 
-if query:
+if st.button("Start Recording"):
+    st.write("Recording... Speak now!")
+    audio = sd.rec(int(DURATION * SAMPLING_RATE), samplerate=SAMPLING_RATE, channels=1, dtype='float32')
+    sd.wait()
+    st.write("Transcribing...")
+
+    # Transcribe with your custom ASR function
+    transcript = asr.transcribe((SAMPLING_RATE, audio))
+    st.success("Transcription:")
+    st.write(transcript)
+
+
+query = st.chat_input("Ask about schemes, benefits, eligibility...", key = "user_input")
+user_input = transcript if transcript else query
+
+if user_input:
     # Retrieve relevant schemes
-    search_results = query_index(query)
+    search_results = query_index(user_input)
     retrieved_info = "\n\n".join(
         f"Scheme: {m.metadata.get('scheme_name', '')}\nDetails: {m.metadata}" for m in search_results.matches
     )
@@ -34,11 +62,11 @@ if query:
     messages += st.session_state.chat_history
     messages.append({
         "role": "user",
-        "content": f"{query}\n\nContext:\n{retrieved_info}"
+        "content": f"{user_input}\n\nContext:\n{retrieved_info}"
     })
 
     # Save user query
-    st.session_state.chat_history.append({"role": "user", "content": query})
+    st.session_state.chat_history.append({"role": "user", "content": user_input})
 
     # Call Groq
     with st.spinner("Boudhi is thinking..."):
@@ -54,10 +82,6 @@ if query:
 
         # Save assistant reply
         st.session_state.chat_history.append({"role": "assistant", "content": answer})
-#---
-#Translation layer.
-#---
-
 
 # Show messages
 for msg in st.session_state.chat_history:
